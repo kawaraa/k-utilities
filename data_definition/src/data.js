9bdc60fac5a52164e8160a4645659521efeba9dd
Ahmad Kawara <kawaraa4@gmail.com>
2020-09-04 15:36:59 +0200

    T

--- data_definition/src/data.js
+++
@@ -1,8 +0,0 @@
-const events = {
-  LOG_IN: "LOGIN",
-  LOG_OUT: "LOGOUT",
-  TYPE: "TYPE",
-  STOP_TYPING: "STOP_TYPING",
-  MESSAGE: "MESSAGE",
-  PRIVATE: "PRIVATE"
-};
--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const Crypto = require("./crypto/crypto");
 const Hashing = require("./crypto/hashing");
 const Request = require("./request/request");
 const Logger = require("./logger/logger");
+const Validator = require("./validator/validator");
 
 module.exports = {
   Crypto,
   Hashing,
+  Validator,
   Request,
   Logger,
 };
