c703a7d824771093931b908c17238db94b0237cb
A Kawara <kawaradev@gmail.com>
2021-02-05 16:41:35 +0100

    Added CustomError class

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const Crypto = require("./crypto/crypto");
 const Hashing = require("./crypto/hashing");
 // const Request = require("./request/request");
 // const Socket = require("./web-socket/websocket");
-const Logger = require("./logger/logger");
+const CustomError = require("./custom-error/custom-error");
 const Validator = require("./validator/validator");
+const Logger = require("./logger/logger");
 const { countries } = require("../config/config.json");
 
 module.exports = {
   Crypto,
   Hashing,
+  CustomError,
   Validator,
   Logger,
   countries,
