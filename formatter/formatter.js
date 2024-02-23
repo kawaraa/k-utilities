3e1faa8e0de32db7beaec4bc4a18e81761e0503e
Kawara <39919017+kawaraa@users.noreply.github.com>
2024-02-23 09:56:47 +0100

    Added getRandomHexColor function.

--- formatter/formatter.js
+++ formatter/formatter.js
@@ -4,6 +4,18 @@ class Formatter {
   static newId() {
     return (Math.random() + Date.now() + Math.random() + "").replace(".", "");
   }
+  static getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 0xffffff)
+      .toString(16)
+      .padEnd(6, "0")}`;
+
+    // // Second way: Generate random values for RGB components
+    // const componentHex = () =>
+    //   Math.floor(Math.random() * 256)
+    //     .toString(16)
+    //     .padStart(2, "0");
+    // return "#" + componentHex() + componentHex() + componentHex();
+  }
 
   static objectToURLEncoded(obj) {
     const query = [];
