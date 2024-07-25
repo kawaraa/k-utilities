81138c66b4a16716d73dafab20565af4d991bc00
Kawara <39919017+kawaraa@users.noreply.github.com>
2024-07-25 16:20:05 +0200

    Added timestamp to Logger class.

--- logger/logger.js
+++ logger/logger.js
@@ -1,17 +1,22 @@
 "use strict";
 
 class Logger {
-  constructor(name = "") {
+  constructor(name = "", includeTimestamp) {
     this.name = name;
+    this.withTime = includeTimestamp;
+  }
+  #getTime() {
+    const d = new Date();
+    return !this.withTime ? "" : `${d.toJSON().substring(0, 10)}-${d.toLocaleTimeString()}`;
   }
   info() {
-    console.log(`[###]-[${this.name}] `, ...arguments);
+    console.log(`[###]-[${this.name}]${this.#getTime()} `, ...arguments);
   }
   warn() {
-    console.warn(`[!!!]-[${this.name}] `, ...arguments);
+    console.warn(`[!!!]-[${this.name}]${this.#getTime()} `, ...arguments);
   }
   error() {
-    console.error(`[XXX]-[${this.name}] `, ...arguments);
+    console.error(`[XXX]-[${this.name}]${this.#getTime()} `, ...arguments);
   }
 }
 
