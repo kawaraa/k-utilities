3eeb00d1a5761cc87a554b6943c27cee6eeb55c8
Kawara <39919017+kawaraa@users.noreply.github.com>
2025-10-02 14:52:18 +0200

    Removed React components.

--- react/tailwind/other.js
+++
@@ -1,5 +0,0 @@
-// bg-black/50 - backdrop-blur-sm
-// odd:bg-neutral-100 odd:dark:bg-neutral-800
-// "truncate" class cutes the text and tail it with ...
-// export const centerShadow = "shadow-[0_0_8px_rgba(0,0,0,0.08)]"; // shadow-[0_0_10px_rgba(0,0,0,0.1)]
-const redStarCls = "after:ml-0.5 after:text-red-500 after:content-['*']";
